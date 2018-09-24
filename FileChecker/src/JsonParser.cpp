d5b001bf550668c517173915d1d8dd84fbe2faa0
Joffrey <32282295+InflamedBanana@users.noreply.github.com>
2018-09-24 19:35:36 +0200

    clean

---
+++ FileChecker/src/JsonParser.cpp
@@ -0,0 +1,92 @@
+#include "JsonParser.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "fileManipulator.h"
+
+using namespace std;
+using namespace rapidjson;
+
+void JSON_Parser::GenerateConfigFile( const string& filePath )
+{
+	std::ofstream configFile( filePath );
+
+	configFile <<
+		"{\n"
+		"	\"FileChecker configs\":\n"
+		"	{\n"
+		"		\"Nomenclature\":\n"
+		"		{\n"
+		"			\"Separator\" : \"_\",\n"
+		"			\"Definitions\":\n"
+		"			{\n"
+		"				\"Prefix\": [\"\"],\n"
+		"				\"Type\" : [\"\"],\n"
+		"				\"Name\" : []\n"
+		"			}\n"
+		"		},\n"
+		"\n"
+		"		\"Associated_Files\" : [],\n"
+		"		\"Directory_Names_Exceptions\" : []\n"
+		"	},\n"
+		"\n"
+		"	\"Arborescence\":\n"
+		"	{\n"
+		"		\"Directories\":\n"
+		"		[\n"
+		"			{\n"
+		"				\"Name\":\"../\",\n"
+		"				\"Exclude_Nomenclature_Check\" : false,\n"
+		"				\"Exclude_Extension_Check\" : false,\n"
+		"				\"Exclude_Recursive_Checks\" : false,\n"
+		"				\"ExtensionRestrict\" :\n"
+		"				[\n"
+		"				],\n"
+		"				\"Directories\" : \n"
+		"				[\n"
+		"					{\n"
+		"						\"Name\":\"Template\",\n"
+		"						\"Exclude_Nomenclature_Check\" : true,\n"
+		"						\"Exclude_Extension_Check\" : true,\n"
+		"						\"Exclude_Recursive_Checks\" : true,\n"
+		"						\"ExtensionRestrict\" :\n"
+		"						[\n"
+		"						],\n"
+		"						\"Directories\" : []\n"
+		"					}\n"
+		"				]\n"
+		"			}\n"
+		"		]\n"
+		"	}\n"
+		"}" << endl;
+
+	configFile.close();
+}
+
+bool JSON_Parser::ParseFile( const std::string& _filePath, Document* _doc )
+{
+	assert( _doc != nullptr );
+	if( !FileManipulator::PathExists( _filePath ) )
+		GenerateConfigFile( _filePath );
+
+	fstream file( _filePath );
+
+	if( !file ) return nullptr;
+
+	string json;
+
+	{
+		string temp;
+		while( getline( file, temp ) )
+			json.append( temp );
+	}
+	
+	_doc->Parse( json.c_str() );
+
+	return !_doc->IsNull();
+}
+
+bool JSON_Parser::CheckBoolValue( const rapidjson::Value & _value, const char* _name )
+{
+	return _value.HasMember(_name) && _value[_name].GetBool();
+}
---
+++ FileChecker/src/fileManipulator.cpp
@@ -0,0 +1,118 @@
+#include "fileManipulator.h"
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+
+namespace FileManipulator
+{
+	using namespace std;
+
+	vector<std::string> GetFilesInDirectory( const string &directory )
+	{
+		vector<std::string> files;
+
+		for( const auto& file : fs::directory_iterator( directory ) )
+			if( fs::is_regular_file( file ) )
+				files.push_back( file.path().string() );
+
+		return files;
+	}
+
+	vector<std::string> GetDirectoriesAtPath( const string &directory )
+	{
+		vector<std::string> files;
+
+		for( const auto& file : fs::directory_iterator( directory ) )
+		{
+			if( fs::is_directory( file ) )
+			{
+				std::string str( file.path().string() );
+				replace( str.begin(), str.end(), '\\', '/' );
+				files.push_back( str );
+			}
+		}
+
+		return files;
+	}
+
+	std::string GetDirectoryAtPath( const string& _directoryPath, const string& _directoryName )
+	{
+		for( const auto& file : fs::directory_iterator( _directoryPath ) )
+			if( fs::is_directory( file ) && file.path().filename().compare( _directoryName ) == 0 )
+				return file.path().string();
+
+		return "";
+	}
+
+	set<fs::path> GetAllFilesInRecursiveDirectory( const string &directory, const std::set<fs::path> *exceptions )
+	{
+		set<fs::path> files;
+
+		for( auto& file : fs::recursive_directory_iterator( directory ) )
+		{
+			if( exceptions != nullptr && exceptions->find( file ) != exceptions->end() )
+				continue;
+			else if( fs::is_regular_file( file ) )
+				files.insert( file );
+		}
+
+		return files;
+	}
+
+	bool DirectoryContainsFile( const std::string& _directoryPath, const std::string& _fileName )
+	{
+		fs::path directory( _directoryPath ), fileToFind( _fileName );
+
+		for( auto& file : fs::directory_iterator( directory ) )
+			if( file.path() == fileToFind )
+				return true;
+
+		return false;
+	}
+
+	bool RenameFile( const std::string &_filePath, const string &newName )
+	{
+		fs::path filePath( _filePath );
+		fs::path newFilePath( _filePath );
+		newFilePath.replace_filename( newName );
+
+		error_code error;
+
+		fs::rename( filePath, newFilePath, error );
+		return error.value() == 0;
+	}
+
+	bool MoveFile( const std::string &_filePath, const std::string &_destinationPath )
+	{
+		error_code error;
+		fs::path file( _filePath ), destination( _destinationPath );
+
+		fs::rename( file, destination / file.filename(), error );
+		return error.value() == 0;
+	}
+
+	bool CreateDirectory( const std::string& _dirPath )
+	{
+		error_code error;
+		fs::create_directories( _dirPath, error );
+
+		return error.value() == 0;
+	}
+
+	bool PathExists( const std::string & _path )
+	{
+		return fs::exists( _path );;
+	}
+
+	bool CreateTextFile( const std::string& _path, const std::string& _name, const std::stringstream& _textStream )
+	{
+		ofstream newFile( _path + _name + ".txt" );
+
+		if( !newFile )
+			return false;
+		
+		newFile << _textStream.rdbuf();
+		newFile.close();
+		return true;
+	}
+}
